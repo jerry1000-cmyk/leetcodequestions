50af9a48b05f10616ddb4ccb42fa8d3eb0b9b0bd
Shounak <shounak.abhijit@gmail.com>
2025-10-08 22:31:00 +0530

    MADE SOME MORE CHANGED 8/10/20205

--- watermelon.cpp
+++ watermelon.cpp
@@ -1,4 +1,20 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
+int main(){
+int w;
+cin >> w;
 
-float w;
+if(w == 2){
+    cout<<"NO";
+}
+else{
+if( w%2 == 0){
+   cout<<"YES";
+}
+else{
+    cout<<"NO";
+}
+}
+
+}
