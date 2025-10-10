2f092177ab35c5f63d0109e59477823fc26ca968
Shounak <shounak.abhijit@gmail.com>
2025-10-10 22:39:59 +0530

    10/10/2025

---
+++ 4sum.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+using namespace std;
+
+
+
+
+vector<vector<int>> fourSum(vector<int>& nums, int target) {
+       vector<vector<int>> ans;
+       int n = nums.size();
+       sort(nums.begin(),nums.end());
+
+       for(int i = 0 ; i<n ; i++){
+        if(i>0 && nums[i] == nums[i-1]) continue;
+
+        for(int j = i+1;j<n;)
+        {
+            int p = j+1,q=n-1;
+            
+            while(p<q){
+                long long sum = (long long)nums[i] + (long long)nums[j] + (long long)nums[p] + (long long)nums[q];
+                if(sum == target){
+                    ans.push_back({nums[i] , nums[j] , nums[p] , nums[q]});
+                    p++;
+                    q--;
+                    while(p<q && nums[p] == nums[p-1]) p++;
+                }
+                else if(sum < target){
+                    p++;
+                }
+                else{
+                    q--;
+                }
+            }
+            j++;
+            while(j<n && nums[j] == nums[j-1]) j++;
+        }
+        
+    }
+       return ans; 
+}
+
+
+int main(){
+    vector<int> nums = {1,0,-1,0,-2,2};
+    int target = 0;
+    vector<vector<int>> ans = fourSum(nums , target);
+    for (auto &triplet : ans) {
+        for (auto &x : triplet) cout << x << " ";
+        cout << endl;
+    }
+    system("pause");
+
+}
---
+++ subarraysumequalsk.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <unordered_map>
+using namespace std;
+
+int subarraySum(vector<int>& arr, int k) {
+        int n = arr.size();
+        int count = 0;
+        vector<int> prefixSum(n,0);
+        
+        prefixSum[0] = arr[0];
+        for(int i = 1 ; i<n ; i++){
+            prefixSum[i] = prefixSum[i-1] + arr[i];
+        }
+        unordered_map<int , int> m;
+
+        for( int j =0 ; j<n ; j++){
+            if(prefixSum[j] == k){
+                count++;
+            }
+            int val = prefixSum[j] - k;
+            if(m.find(val) != m.end()){
+                count += m[val];
+            }
+            if(m.find(prefixSum[j]) == m.end()){
+                m[prefixSum[j]] = 0;
+            }
+            m[prefixSum[j]]++;
+
+        }
+       return count; 
+    }
+
+
+int main(){
+    vector<int> nums = {1,2,3};
+    int k = 3;
+    cout << subarraySum(nums , k) <<endl;
+    system("pause");
+}
